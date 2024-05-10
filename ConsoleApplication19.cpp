93068c72f9a9f768c672fcb8611ddf5561a5e18c
scvrsecvrse <pshzxcursed@gmail.com>
2024-05-10 15:51:00 +1100

    asd

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <tgbot/tgbot.h>
+
+int main() {
+    TgBot::Bot bot("7090235342:AAGTsvzqhAdApeIdMMjuKNJ7pBD20TLQCkM");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (message->text.find("u looser") != std::string::npos) {
+            bot.getApi().sendMessage(message->chat->id, "I got");
+            return;
+        }
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+
+        });
+        
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
